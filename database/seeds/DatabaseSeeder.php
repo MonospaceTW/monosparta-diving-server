7016a275d6ea976435b0b1845b0c805563390553
hikarunost <hikarunost@gmail.com>
2019-08-13 03:36:20 +0800

    open seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,20 +15,20 @@ class DatabaseSeeder extends Seeder
     {
         factory(App\User::class, 10)->create();
 
-        // factory(App\Spot::class, 10)->create();
+        factory(App\Spot::class, 10)->create();
 
-        // factory(App\Shop::class, 10)->create();
+        factory(App\Shop::class, 10)->create();
 
-        // factory(App\Comment::class, 1000)->create();
+        factory(App\Comment::class, 1000)->create();
 
         factory(App\Article::class, 25)->create();
 
-        // DB::table('shop_comments')->insert(
-        //     [
-        //         'shop_id' => App\Shop::select('id')->orderByRaw("RAND()")->first()->id,
-        //         'comment_id' => App\Comment::select('id')->orderByRaw("RAND()")->first()->id,
-        //     ]
-        // );
+        DB::table('shop_comments')->insert(
+            [
+                'shop_id' => App\Shop::select('id')->orderByRaw("RAND()")->first()->id,
+                'comment_id' => App\Comment::select('id')->orderByRaw("RAND()")->first()->id,
+            ]
+        );
 
     }
 }
